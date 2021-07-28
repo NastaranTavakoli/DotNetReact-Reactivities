82e70cb9a6351c4c9282fd287869c0841c74985d
nastaran-tavakoli <nastaran.tavakoli.91@gmail.com>
2021-07-28 17:30:58 +1000

    Add seed class and Seed initial data

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             //var host = CreateHostBuilder(args).Build().Run();
             var host = CreateHostBuilder(args).Build();
@@ -28,15 +28,16 @@ namespace API
                 var context = services.GetRequiredService<DataContext>();
                 //get ahold of the DataContext that we added as a service in the startup class 
 
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
                 //add migrations to the existing database and if database does not exist, the database will be created
+                await Seed.SeedData(context);
             }
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occurred during migration");
             }
-            host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
